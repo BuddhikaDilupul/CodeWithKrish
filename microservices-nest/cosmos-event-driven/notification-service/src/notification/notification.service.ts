94a9cd0f4828868043bfbe2545dde6c632e175d7
Buddhika <buddhikadilupulr@gmail.com>
2025-03-04 00:21:49 +0530

    event driven -- \notification service added and added log to display notification

---
+++ microservices-nest/cosmos-event-driven/notification-service/src/notification/notification.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Kafka } from 'kafkajs';
+@Injectable()
+export class NotificationService implements OnModuleInit {
+  private readonly Kafka = new Kafka({ brokers: ['localhost:9092'] });
+  private readonly producer = this.Kafka.producer();
+  private readonly consumer = this.Kafka.consumer({
+    groupId: 'buddhika-notification-service',
+  });
+  constructor() {}
+  async onModuleInit() {
+    await this.producer.connect();
+    await this.consumer.connect();
+    await this.consumeNotification();
+  }
+
+  // consume notification
+  async consumeNotification() {
+    await this.consumer.subscribe({
+      topic: 'buddhika.order.confirmed',
+    });
+
+    await this.consumer.run({
+      eachMessage: async ({ message }) => {
+        if (message.value !== null) {
+          const msg = JSON.parse(message.value.toString());
+          // Display notification
+          console.log(msg.message);
+        }
+      },
+    });
+  }
+}
---
+++ microservices-nest/cosmos-event-driven/order-service/src/orders/orders.service.ts
@@ -0,0 +1,146 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './entity/order.entity';
+import { Repository } from 'typeorm';
+import { OrderItem } from './entity/order-item.entity';
+import { createOrderDto } from './dto/create-order.dto';
+import { OrderStatus, UpdateOrderStatus } from './dto/update-order.dto';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
+import { Kafka } from 'kafkajs';
+@Injectable()
+export class OrdersService implements OnModuleInit {
+  private readonly Kafka = new Kafka({ brokers: ['localhost:9092'] });
+  private readonly producer = this.Kafka.producer();
+  private readonly consumer = this.Kafka.consumer({
+    groupId: 'buddhika-order-service',
+  });
+  private readonly inventoryServiceUrl = 'http://localhost:3001/products';
+  private readonly customerServiceUrl = 'http://localhost:3002/customers';
+
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+    @InjectRepository(OrderItem)
+    private readonly orderItemRepository: Repository<OrderItem>,
+    private readonly httpService: HttpService,
+  ) {}
+  async onModuleInit() {
+    await this.producer.connect();
+    await this.consumer.connect();
+    await this.consumerConfirmedOrder();
+  }
+
+  async create(createOrderDto: createOrderDto): Promise<any> {
+    const { customerId, items } = createOrderDto;
+
+    // Validate customer exists
+    let customerName = '';
+    try {
+      const response$ = this.httpService.get(
+        `${this.customerServiceUrl}/${customerId}`,
+      );
+      const response = await lastValueFrom(response$);
+      customerName = response.data.name;
+    } catch (error) {
+      throw new BadRequestException(
+        `Customer ID ${customerId} does not exist.`,
+      );
+    }
+
+    // produce odrer as an event
+
+    this.producer.send({
+      topic: 'buddhika.order.create',
+      messages: [
+        {
+          value: JSON.stringify({ customerId, customerName, items }),
+        },
+      ],
+    });
+
+    return { message: 'Order is placed. waiting invemtory service to process' };
+  }
+
+  async fetch(id: any) {
+    return await this.orderRepository.findOne({
+      where: { id },
+      relations: ['items'],
+    });
+  }
+
+  async fetchAll() {
+    return await this.orderRepository.find({ relations: ['items'] });
+  }
+
+  async updateOrderStaus(id: number, updateStatus: UpdateOrderStatus) {
+    const order = await this.orderRepository.findOne({ where: { id } });
+    if (!order) {
+      throw new NotFoundException(`order with id: ${id} is not found`);
+    }
+    if (
+      order.status === OrderStatus.DELIVERED ||
+      order.status === OrderStatus.CANCELLED
+    ) {
+      throw new BadRequestException(
+        `order status cannot be changed when its delivered or cancelled`,
+      );
+    }
+    order.status = updateStatus.status;
+    return await this.orderRepository.save(order);
+  }
+
+  // Save order to db
+  async saveOrder(createOrderDto: createOrderDto, customerName: string) {
+    try {
+      const { customerId, items } = createOrderDto;
+      const order = this.orderRepository.create({
+        customerId,
+        status: 'PENDING',
+      });
+      const savedOrder = await this.orderRepository.save(order);
+
+      const orderItems = items.map((item) =>
+        this.orderItemRepository.create({
+          productId: item.productId,
+          price: item.price,
+          quantity: item.quantity,
+          order: savedOrder,
+        }),
+      );
+      const savedOrderItems = await this.orderItemRepository.save(orderItems);
+      await this.producer.send({
+        topic: 'buddhika.order.confirmed',
+        messages: [
+          {
+            value: JSON.stringify({
+              message: `${customerName}'s Order is placed and confirmed succcessfully. Order Id is ${savedOrder.id}`,
+            }),
+          },
+        ],
+      });
+      
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  async consumerConfirmedOrder() {
+    await this.consumer.subscribe({
+      topic: 'buddhika.order.inventory.update',
+    });
+    await this.consumer.run({
+      eachMessage: async ({ message }) => {
+        const { customerId, customerName, items } = JSON.parse(
+          message.value.toString(),
+        );
+        await this.saveOrder({ customerId, items }, customerName);
+      },
+    });
+  }
+}
